28a7f0c568cfc3c0a4d92ccb1d17d9305bd3bf1e
GamingLiamStudios <58615717+GamingLiamStudios@users.noreply.github.com>
2023-03-25 23:01:06 +1100

    prepare for crates.io

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # BMF-Parse
 
 An ISO-BMFF/MP4 parser. Read Only, Write capabilities maybe later.
+Oh and no docs either, so have fun i guess lol.
+Doesn't support the entire spec either, cause this is all i need. may add more later.
--- examples/simple.rs
+++ examples/simple.rs
@@ -1,9 +1,21 @@
-use bmf_parse::*;
+use bmf_parse::{boxes::*, *};
 
 fn main() {
-    let box_ = parse_mp4(&[
+    let mp4 = parse_mp4(&[
         0x00, 0x00, 0x00, 0x18, b'm', b'o', b'o', b'f', 0x00, 0x00, 0x00, 0x10, b'm', b'f', b'h',
         b'd', 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
     ]);
-    println!("{:#?}", box_);
+    println!("{:#?}", mp4);
+
+    let moof = find_box(mp4.as_slice(), b"moof").unwrap();
+    let moof = match moof {
+        Mp4Box::Moof(moof) => &**moof,
+        _ => panic!("Not a BoxMoof!"),
+    };
+
+    let mfhd = find_box(moof.data.as_slice(), b"mfhd").unwrap();
+    let mfhd = match mfhd {
+        Mp4Box::Mfhd(mfhd) => &**mfhd,
+        _ => panic!("Not a BoxMfhd!"),
+    };
 }
